26a78f29867026bade793c0b1eb17780a46ed34a
satyam-fascave <satyam.u@fascave.com>
2025-09-30 22:53:50 +0530

    fixed dropdown

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -35,25 +35,19 @@ const buttonVariants = cva(
   },
 )
 
-function Button({
-  className,
-  variant,
-  size,
-  asChild = false,
-  ...props
-}: React.ComponentProps<'button'> &
-  VariantProps<typeof buttonVariants> & {
-    asChild?: boolean
-  }) {
+const Button = React.forwardRef<
+  HTMLButtonElement,
+  React.ComponentProps<'button'> &
+    VariantProps<typeof buttonVariants> & { asChild?: boolean }
+>(({ className, variant, size, asChild = false, ...props }, ref) => {
   const Comp = asChild ? Slot : 'button'
-
   return (
     <Comp
-      data-slot="button"
+      ref={ref} // <---- Forward ref here
       className={cn(buttonVariants({ variant, size, className }))}
       {...props}
     />
   )
-}
+})
 
 export { Button, buttonVariants }
