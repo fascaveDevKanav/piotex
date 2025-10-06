9dfccd0ae54956a232fea52f7aef848dffc33afc
satyam-fascave <satyam.u@fascave.com>
2025-10-06 12:25:27 +0530

    scroller animation added

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import Link from "next/link"
 
 import  {categories}  from "@/lib/data"
 import ProductGrid from "@/components/product/productcard"
+import { InfiniteSlider } from "@/components/ui/infinite-slider"
 
 
 export default function HomePage() {
@@ -41,10 +42,10 @@ export default function HomePage() {
         {/* Featured Categories */}
         <div className="mb-12">
           <h2 className="text-2xl font-bold text-gray-900 mb-8 text-center">Shop by Category</h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+            <InfiniteSlider durationOnHover={80} gap={100}>
             {categories.map((category) => (
               <div key={category.id}  className="group">
-                <Card className="h-48 bg-gradient-to-br from-pink-100 to-purple-100 hover:shadow-lg transition-all duration-300 group-hover:scale-105">
+                <Card className="h-48 w-72 bg-gradient-to-br from-pink-100 to-purple-100 hover:shadow-lg transition-all duration-300 group-hover:scale-105">
                   <CardContent className="flex items-center justify-center h-full">
                     <div className="text-center">
                       <h3 className="text-xl font-semibold text-gray-900 group-hover:text-pink-600 transition-colors">
@@ -56,7 +57,7 @@ export default function HomePage() {
                 </Card>
               </div>
             ))}
-          </div>
+            </InfiniteSlider>          
         </div>
 
         {/* Featured Products */}
