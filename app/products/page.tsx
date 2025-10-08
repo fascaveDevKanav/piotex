1549fcd82e0b58403ba0640cb2f1449edfe1c0a4
kanav789 <kanavrana91@gmail.com>
2025-10-08 15:36:17 +0530

    did the changes in product list

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -6,17 +6,23 @@ import { Allproducts } from "@/lib/products";
 import { useEffect } from "react";
 
 import { useProduct } from "@/hooks/usedata-product";
+import { getListData } from "@/lib/customfetch/customFetch";
+import { useDispatch, useSelector } from "react-redux";
+import endpoints from "@/lib/endpoints/endponts";
 export default function ProductPage(){
-    const { productData, setProductData } = useProduct()
- 
-   useEffect(()=>{
-  const fetchData = async()=>{
-  const data = await Allproducts();
-  setProductData(data?.products)
-  }
-  fetchData()
-   },[setProductData])
+ const {products} = useSelector((state:any)=> state?.reduxData?.data)
 
+console.log("products", products?.products)
+  const dispatch = useDispatch()
+   useEffect(()=>{
+    fetchData()
+   },[])
+
+   const fetchData = async ()=>{
+    
+    await getListData(dispatch, "products", endpoints?.products?.allproduct)
+
+   }
    return(
      <div className=" bg-gray-50">
       <Header />
@@ -25,7 +31,7 @@ export default function ProductPage(){
      <div className="container ">
        <div className="container mx-auto px-4 py-5">
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-               {productData ? productData.map((p) => (
+               {products?.products ? products?.products.map((p) => (
                 <ProductCard key={p.id} product={p} />
               )) : <p className="flex justify-center">No products found</p>}
             </div>
